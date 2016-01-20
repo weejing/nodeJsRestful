cc8b7fe5f576e180afd3fce39b0f903498975989
Chung wee jing <WeeJing@Chungs-MacBook-Pro.local>
2016-01-21 02:13:57 +0800

    edit

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var mongoose = require('mongoose');
 var bodyParser = require ('body-parser');
 
 // MongoDB
-mongoose.connect('mongodb://localhost/rest_test');
+mongoose.connect(process.env.MONGOLAB_URI, function(error){
+	if (error) console.error(error);
+	else console.log('mongo connected');
+});
 
 //Express
 var app = express();
