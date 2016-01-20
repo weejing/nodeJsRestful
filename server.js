530c98517245495862a0020ca58f2746e304e8a6
Chung wee jing <WeeJing@Chungs-MacBook-Pro.local>
2016-01-20 23:08:03 +0800

    edit server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var bodyParser = require ('body-parser');
 
 // MongoDB
-mongoose.connect('mongodb://localhost/rest_test');
+//mongoose.connect('mongodb://localhost/rest_test');
 
 //Express
 var app = express();
