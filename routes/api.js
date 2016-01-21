a70e3d4ec11a9ed16fb34f73e99262061f73218c
Chung wee jing <WeeJing@Chungs-MacBook-Pro.local>
2016-01-22 00:08:30 +0800

    edit

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,15 @@ var router = express.Router();
 var Product = require('../models/product');
 
 // Routes
-//Product.methods(['get','put','post', 'delete']);
-//Product.register(router,'/products')
+Product.methods(['get','put','post', 'delete']);
+Product.register(router,'/products')
 
-app.get('/products', function(req,res){
+/*app.get('/products', function(req,res){
 	Product.find(function (err, data){
 		res.json(data);
 	});
-
 });
-
+*/
 
 // return router
 module.exports = router;
