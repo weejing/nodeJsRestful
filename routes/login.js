4b92f9c3f81b5c502208ab8b35e467202b441d55
Chung wee jing <WeeJing@r-249-100-25-172.comp.nus.edu.sg>
2016-01-23 18:38:10 +0800

    edit

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ var users = require('../models/users');
 
 router.get('/:username/:password/:role', function(req,res) {
 	users.find(req.params.username,req.params.role,function(err,data){
-		res.json(data);
+		console.log(data);
 	})
 
 });
